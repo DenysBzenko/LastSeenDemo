0a15b6006d9b7e5cea4b46d577e158b8725407c8
Denys Bzenko <dbzenko@kse.org.ua>
2023-11-08 21:37:24 +0200

    fix

--- LastSeenDemo.API/Program.cs
+++ LastSeenDemo.API/Program.cs
@@ -12,7 +12,9 @@ var dateTimeProvider = new DateTimeProvider();
 var loader = new Loader();
 var detector = new OnlineDetector(dateTimeProvider);
 var predictor = new Predictor(detector);
-var userLoader = new UserLoader(loader, "sef.podkolzin.consulting/api/users/lastSeen");
+#pragma warning disable S1075
+var userLoader = new UserLoader(loader, "https://sef.podkolzin.consulting/api/users/lastSeen");
+#pragma warning restore S1075
 var application = new LastSeenApplication(userLoader);
 var userTransformer = new UserTransformer(dateTimeProvider);
 var allUsersTransformer = new AllUsersTransformer(userTransformer);
