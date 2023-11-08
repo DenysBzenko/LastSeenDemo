7f6faac9f317cbce8125f4fcaace2ea8595d3bab
Denys Bzenko <dbzenko@kse.org.ua>
2023-11-08 11:55:00 +0200

    Fix

---
+++ LastSeenDemo.API/ReportItem.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="ReportItem.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace LastSeenDemo.API
+{
+    public class ReportItem
+    {
+        public Guid UserId { get; set; }
+
+        public double Total { get; set; }
+
+        public double DailyAverage { get; set; }
+
+        public double WeeklyAverage { get; set; }
+
+        public double Min { get; set; }
+
+        public double Max { get; set; }
+    }
+}
